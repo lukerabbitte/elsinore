47303df044f5095b2d1ab84aba1860d8fa2ebcdb
lukerabbitte <luke.rabbitte@gmail.com>
2025-02-04 16:19:45 +0000

    Removes premature auth endpoint

--- src/app/api/auth/route.js
+++ src/app/api/auth/route.js
@@ -1,15 +1,5 @@
 import { supabase } from "@/lib/supabase";
 
 export async function GET() {
-    const { data, error } = await supabase.auth.signInWithPassword({
-        email: "luke.rabbitte@gmail.com",
-        password: "Octopus9909",
-    });
-
-    if (error) {
-        console.error("Auth error:", error);
-        return;
-    }
-
-    console.log("Bearer Token:", data.session.access_token);
+    return new Response(JSON.stringify("Hello Auth"), { status: 200 });
 }
