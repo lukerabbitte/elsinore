00c146fcf81dd70f2a3c9ae2fcbc75362839ec75
lukerabbitte <luke.rabbitte@gmail.com>
2025-02-11 17:01:30 +0000

    Fixes margin

--- src/components/HighlightCard.jsx
+++ src/components/HighlightCard.jsx
@@ -22,7 +22,7 @@ const HighlightCard = ({
     };
 
     return (
-        <div className="bg-gradient-radial rounded-xl h-2/3 sm:h-full max-w-[65ch] mx-8 sm:mx-0 p-4 flex flex-col gap-4 justify-between items-center">
+        <div className="bg-gradient-radial rounded-xl h-2/3 sm:h-full max-w-[65ch] mx-4 sm:mx-0 p-4 flex flex-col gap-4 justify-between items-center">
             <div className="flex flex-row items-center justify-between w-full gap-4">
                 <h1 className="font-semibold lg:font-extrabold text-foreground text-2xl text-balance">
                     {highlight.title}
